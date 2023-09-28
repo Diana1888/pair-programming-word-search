bb3753a461a4d2b546219998c5ebf491e8355df8
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-09-27 22:42:51 -0400

    Add word search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    
+    const horizontalJoin = letters.map(letter => letter.join(''))
+
+    const verticalJoin = letters.map((value, index) => letters.map(row => row[index]).join(''))
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
 }
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
